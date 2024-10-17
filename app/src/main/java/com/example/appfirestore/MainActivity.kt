cf5ad85de78d3e61b8f6f6ec33406ef833a227ca
joaoenrique <jebsantosalves@gmail.com>
2024-10-17 19:49:26 -0300

    feat: listar

--- app/src/main/java/com/example/appfirestore/MainActivity.kt
+++ app/src/main/java/com/example/appfirestore/MainActivity.kt
@@ -1,31 +1,32 @@
 package com.example.appfirestore
 
 import android.annotation.SuppressLint
-import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.benchmark.perfetto.Row
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appfirestore.ui.theme.AppFirestoreTheme
 import com.google.firebase.Firebase
@@ -140,23 +141,48 @@ fun App(db: FirebaseFirestore) {
         Row(Modifier.fillMaxWidth().padding(20.dp)) {
 
         }
+
         Row(Modifier.fillMaxWidth()) {
-            Column(Modifier.fillMaxWidth(0.3f)) {
-            Text(text = "Nome:")}
+            Column(Modifier.fillMaxWidth(0.5f)) {
+                Text(text = "Nome:")
             }
-            Row(Modifier.fillMaxWidth()) {
-                Column(            ) {
-                    db.collection("Clientes").get().addOnSuccessListener { documents ->
-                        for (document in documents) {
-                            val lista = hashMapOf(
-                                "nome" to "${document.data.get("nome")}",
-                                "telefone" to "${document.data.get("telefone")}")
-                            Log.d(TAG, "${document.id} => ${document.data}")}
+            Column(Modifier.fillMaxWidth(0.5f)) {
+                Text(text = "Telefone:")
+            }
+        }
+        Row(Modifier.fillMaxWidth()) {
+            val clientes = remember { mutableStateListOf<HashMap<String, String>>() }
+
+            // Buscando os dados do Firestore
+            LaunchedEffect(Unit) {
+                db.collection("Clientes").get().addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        val lista = hashMapOf(
+                            "nome" to "${document.getString("nome") ?: "--"}",
+                            "telefone" to "${document.data["telefone"] ?: "--"}"
+                        )
+                        clientes.add(lista)
                     }
-                        .addOnFailureListener { exception ->
-                            Log.w(TAG, "Error getting documents: ", exception)
-                        }
+                }.addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents: ", exception)
                 }
             }
+
+
+            Column(modifier = Modifier.fillMaxWidth()) {
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                    items(clientes) { cliente ->
+                        Row(Modifier.fillMaxWidth()) {
+                            Column(Modifier.weight(0.5f)) {
+                                Text(text = cliente["nome"] ?: "--")
+                            }
+                            Column(Modifier.weight(0.5f)) {
+                                Text(text = cliente["telefone"] ?: "--")
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
